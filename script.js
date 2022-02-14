35399ce06952373ebab9bc37f0c6d2123d801f60
JZ <73402906+JZSweet@users.noreply.github.com>
2022-02-13 18:22:27 -0800

    set up case study pages

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 function openWindow() {
-    window.open("case1.html");
+    window.open('case1.html');
 };
 
 function openWindow2() {
-    window.open("case2.html");
+    window.open('case2.html');
+};
+
+function openWindow3() {
+    window.open('case3.html');
 };
 
 function closeWindow() {

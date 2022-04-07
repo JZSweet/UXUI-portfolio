b28d0df7a97d8b6748fc37d46dd3c8fa0161cf21
JZ <73402906+JZSweet@users.noreply.github.com>
2022-04-07 13:48:37 -0700

    humberger menue

--- header.js
+++ header.js
@@ -1,11 +1,11 @@
 const img = document.querySelector(".hamburgerMenu");
-const menu = document.querySelector("header-right");
+const menu = document.querySelector(".header-right");
 const open = false;
 img.onclick = () => {
     console.log('clicked')
-    if (menu.style.display === "block") {
+    if (menu.style.display === "flex") {
         menu.style.display = "none";
     } else {
-        menu.style.display = "block";
+        menu.style.display = "flex";
     }
 }
